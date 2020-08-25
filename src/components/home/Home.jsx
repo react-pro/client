195f41d83585bce057e123d60b6a67e82e6d8c5a
Nataliia Mogylchuk <nataliia.mogylchuk@gmail.com>
2020-08-25 17:10:45 +0300

    Implement basic structure

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import './home.scss';
+
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="home__title">Home</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/shared/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './footer.scss';
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p className="footer__title">Footer</p>
+    </div>
+  );
+};
+
+export default Footer;
