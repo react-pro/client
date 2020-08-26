b1bdc62decf4b2f91827e90c0b902954d8774702
Nataliia Mogylchuk <nataliia.mogylchuk@gmail.com>
2020-08-26 16:08:09 +0300

    Add learning-item-book component

--- src/components/learning-portal/components/learning-item-link/Learning-item-link.jsx
+++ src/components/learning-portal/components/learning-item-link/Learning-item-link.jsx
@@ -2,20 +2,19 @@ import './learning-item-link.scss';
 import React from 'react';
 import { Row, Col } from 'reactstrap';
 
-const LearningItemLink = (link) => {
+const LearningItemLink = ({link}) => {
     return (
         <li className="source__item">
             <Row>
                 <Col lg="3" md="3" xs="12">
-                
-                    <img src={`${link.link.image}`} className="source__image"/>
+                    <img src={`${link.image}`} alt={`${link.name}`} className="source__image"/>
                 </Col>
                 <Col lg="9" md="9" xs="12">
-                     <a className="source__link" href={`${link.link.source}`} target='_blank' rel='noopener noreferrer'>
-                        <h4 className="source__name">{link.link.name}</h4>
-                            {link.link.source}
+                    <h4 className="source__name">{link.name}</h4>
+                    <a className="source__link" href={`${link.source}`} target='_blank' rel='noopener noreferrer'>
+                            {link.source}
                     </a>
-                    <p className="source__description">{link.link.description}</p>
+                    <p className="source__description">{link.description}</p>
                 </Col>
             </Row>
         </li>
