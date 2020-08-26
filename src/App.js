29ed74f2a114fbd19d66e49fd6fff872c5e421e8
Nataliia Mogylchuk <nataliia.mogylchuk@gmail.com>
2020-08-26 08:05:33 +0300

    Resolve merge conflicts

--- src/App.js
+++ src/App.js
@@ -8,12 +8,10 @@ import {useDispatch, useSelector} from "react-redux";
 import {alertActions} from "./components/alerts/actions";
 import LoginPage from "./components/login";
 import Header from './shared/header/Header';
-import Footer from './shared/footer/Footer';
 import About from './components/about/About';
 import Quiz from './components/quiz/Quiz';
 import Resume from './components/resume/Resume';
 import Alert from "react-bootstrap/Alert";
-import Test from "./components/test/Test";
 
 const App = () => {
   const alert = useSelector(state => state.alert);
@@ -39,11 +37,10 @@ const App = () => {
           <Route path="/login" component={LoginPage} />
           <Route path="/register" component={RegisterPage} />
           <Route exact path='/about' component={About} />
-          <Route exact path='/test' component={Test} />
+          <Route exact path='/quiz' component={Quiz} />
           <Route exact path='/resume' component={Resume} />
           <Redirect from="*" to="/" />
         </Switch>
-        {/*<Footer />*/}
       </Router>
     </div>
   );
